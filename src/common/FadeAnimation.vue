1c0a9038434366114517ba48df678512fc096d5c
zhangdudu <44902197+z1025238864@users.noreply.github.com>
2019-08-07 18:53:46 +0800

    移动端 去哪

---
+++ src/common/FadeAnimation.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class='fadeanimation'>
+        <slot></slot>
+    </div>
+</template>
+<script>
+ export default {
+    name:'FadeAnimation',
+
+}
+</script>
+
+<style lang='stylus' scoped>
+    .v-enter, .v-leave-to
+        opacity 0
+    .v-enter-active, .v-leave-to-active
+        transition:opacity .5s 
+
+    
+
+</style>
